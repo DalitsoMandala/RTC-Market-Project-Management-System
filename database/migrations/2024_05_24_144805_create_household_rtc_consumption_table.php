70e1e14982af68ffd9c4df6a2fd6cd666692de82
Dalitso Mandala <106370521+DalitsoMandala@users.noreply.github.com>
2024-05-24 21:07:53 +0200

    made tables for rtc consumption HH

---
+++ database/migrations/2024_05_24_144805_create_household_rtc_consumption_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('household_rtc_consumption', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('location_id')->constrained('hrc_locations'); // Link to locations table
+            $table->date('date_of_assessment');
+            $table->enum('actor_type', ['FARMER', 'PROCESSOR', 'TRADER', 'NUTRITION_INTERVENTION', 'OTHER']);
+            $table->string('rtc_group_platform')->nullable(); // Allow null for RTC group/platform
+            $table->string('producer_organisation')->nullable(); // Allow null for producer organization
+            $table->string('actor_name');
+            $table->enum('age_group', ['YOUTH', 'NOT_YOUTH']);
+            $table->enum('sex', ['MALE', 'FEMALE']);
+            $table->string('phone_number')->nullable(); // Allow null for phone number
+            $table->integer('household_size');
+            $table->integer('under_5_in_household');
+            $table->integer('rtc_consumers'); // Renamed for clarity
+            $table->tinyInteger('rtc_consumption_frequency')->unsigned(); // Limit to positive values
+            $table->enum('main_food', ['CASSAVA', 'POTATO', 'SWEET_POTATO']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('household_rtc_consumption');
+    }
+};
