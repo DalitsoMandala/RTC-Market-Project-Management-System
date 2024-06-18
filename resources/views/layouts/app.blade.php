1883ab8ae5ec1eeacaf8600eb2c12ab8cc885e57
Dalitso Mandala <106370521+DalitsoMandala@users.noreply.github.com>
2024-06-18 10:52:11 +0200

    updated filepond from unpkg to jsdelivr

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -91,15 +91,27 @@
             <script src="{{ asset('assets/libs/simplebar/simplebar.min.js') }}"></script>
             <script src="{{ asset('assets/libs/feather-icons/feather.min.js') }}"></script>
             <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-            <script src="https://unpkg.com/filepond-plugin-image-exif-orientation/dist/filepond-plugin-image-exif-orientation.js">
+            <script
+                src="https://cdn.jsdelivr.net/npm/filepond-plugin-image-exif-orientation@1.0.11/dist/filepond-plugin-image-exif-orientation.min.js">
             </script>
-            <script src="https://unpkg.com/filepond-plugin-image-resize/dist/filepond-plugin-image-resize.js"></script>
-            <script src="https://unpkg.com/filepond-plugin-file-encode/dist/filepond-plugin-file-encode.js"></script>
-            <script src="https://unpkg.com/filepond-plugin-image-transform/dist/filepond-plugin-image-transform.js"></script>
-            <script src="https://unpkg.com/filepond-plugin-image-edit/dist/filepond-plugin-image-edit.js"></script>
-            <script src="https://unpkg.com/filepond-plugin-file-validate-type/dist/filepond-plugin-file-validate-type.js"></script>
-            <script src="https://unpkg.com/filepond-plugin-file-validate-size/dist/filepond-plugin-file-validate-size.js"></script>
-            <script src="https://unpkg.com/filepond-plugin-image-preview/dist/filepond-plugin-image-preview.js"></script>
+            <script src="https://cdn.jsdelivr.net/npm/filepond-plugin-image-resize@2.0.10/dist/filepond-plugin-image-resize.min.js">
+            </script>
+            <script src="https://cdn.jsdelivr.net/npm/filepond-plugin-file-encode@2.1.14/dist/filepond-plugin-file-encode.min.js">
+            </script>
+            <script
+                src="https://cdn.jsdelivr.net/npm/filepond-plugin-image-transform@3.8.7/dist/filepond-plugin-image-transform.min.js">
+            </script>
+            <script src="https://cdn.jsdelivr.net/npm/filepond-plugin-image-edit@1.6.3/dist/filepond-plugin-image-edit.min.js">
+            </script>
+            <script
+                src="https://cdn.jsdelivr.net/npm/filepond-plugin-file-validate-type@1.2.9/dist/filepond-plugin-file-validate-type.min.js">
+            </script>
+            <script
+                src="https://cdn.jsdelivr.net/npm/filepond-plugin-file-validate-size@2.2.8/dist/filepond-plugin-file-validate-size.min.js">
+            </script>
+            <script
+                src="https://cdn.jsdelivr.net/npm/filepond-plugin-image-preview@4.6.12/dist/filepond-plugin-image-preview.min.js">
+            </script>
             <script src="https://cdn.jsdelivr.net/npm/filepond@4.31.1/dist/filepond.min.js"></script>
             <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
             <script src="{{ asset('assets/libs/choicesjs/scripts/choices.min.js') }}"></script>
