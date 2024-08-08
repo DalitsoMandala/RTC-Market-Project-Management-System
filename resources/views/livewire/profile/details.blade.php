41425d9255cefcd7801d8965db98d97ea692d26e
Dalitso Mandala <106370521+DalitsoMandala@users.noreply.github.com>
2024-08-09 01:32:45 +0200

    updated profile page breadcrumb

--- resources/views/livewire/profile/details.blade.php
+++ resources/views/livewire/profile/details.blade.php
@@ -5,12 +5,12 @@
         <div class="row">
             <div class="col-12">
                 <div class="page-title-box d-flex align-items-center justify-content-between">
-                    <h4 class="mb-0">Dashboard</h4>
+                    <h4 class="mb-0">Update profile</h4>
 
                     <div class="page-title-right">
                         <ol class="m-0 breadcrumb">
-                            <li class="breadcrumb-item"><a href="javascript: void(0);">Dashboard</a></li>
-                            <li class="breadcrumb-item active">Page Name</li>
+                            <li class="breadcrumb-item"><a href="/">Dashboard</a></li>
+                            <li class="breadcrumb-item active">Profile</li>
                         </ol>
                     </div>
 
