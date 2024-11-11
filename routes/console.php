650270b5a6b2b75c62f8964cd70d10660fedcc1e
“Dalitso <dalitso.mandala.prince@gmail.com>
2024-11-11 11:19:32 +0200

    added another daily scheduler

--- routes/console.php
+++ routes/console.php
@@ -11,3 +11,12 @@ use App\Console\Commands\FetchExchangeRates;
 //Schedule::command('update:information')->dailyAt('00:01');
 Schedule::command('update:information')->dailyAt('00:01');
 Schedule::command('notifications:send-expired-periods')->dailyAt('00:01');
+Schedule::call(function () {
+    $files = glob(public_path('exports/*')); // Get all files in the exports directory
+
+    foreach ($files as $file) {
+        if (is_file($file)) {
+            unlink($file); // Delete each file
+        }
+    }
+})->daily()->at('00:00');
