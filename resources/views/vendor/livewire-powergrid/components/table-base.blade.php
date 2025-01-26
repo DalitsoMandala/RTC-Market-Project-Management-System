74ab727839e41ea268b846ac45b9bf7671bb7629
“Dalitso <dalitso.mandala.prince@gmail.com>
2025-01-26 18:24:02 +0200

    update table size

--- resources/views/vendor/livewire-powergrid/components/table-base.blade.php
+++ resources/views/vendor/livewire-powergrid/components/table-base.blade.php
@@ -8,12 +8,12 @@
 @php
     $baseTableClass = str_replace('table-bordered', '', $theme['table']['tableClass']);
     $headerTableClass = $theme['table']['theadClass'] . ' table-warning';
-    $bodyTableStyle = $theme['table']['tbodyStyle'] . ' font-size: 0.75rem';
+
 @endphp
 
 <div @isset($this->setUp['responsive']) x-data="pgResponsive" @endisset>
     <table id="table_base_{{ $tableName }}" class="table power-grid-table {{ $baseTableClass }}"
-        style="{{ data_get($theme, 'tableStyle') }} ">
+        style="{{ data_get($theme, 'tableStyle') }} font-size: 0.75rem;">
         <thead class="{{ $headerTableClass }}" style="{{ data_get($theme, 'table.theadStyle') }} ">
             {{ $header }}
         </thead>
@@ -22,7 +22,8 @@
                 {{ $body }}
             </tbody>
         @else
-            <tbody class="{{ data_get($theme, 'table.tbodyClass') }}" style="{{ $bodyTableStyle }} ">
+            <tbody class="{{ data_get($theme, 'table.tbodyClass') }}"
+                style="{{ data_get($theme, 'table.tbodyStyle') }} ">
                 {{ $loading }}
             </tbody>
         @endif
