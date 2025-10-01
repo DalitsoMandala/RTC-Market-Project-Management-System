0aa69a89cd59d23a98a4bdca129e965cc94205c1
Dalitso Mandala <dalitso.mandala.prince@gmail.com>
2025-10-01 15:51:34 +0200

    update email blade

--- resources/views/vendor/notifications/email.blade.php
+++ resources/views/vendor/notifications/email.blade.php
@@ -35,7 +35,7 @@
 
 @if(\App\Models\SystemDetail::find(1))
 <p style="color:#FF6600;font-weight: bold;padding-bottom: 0;margin-bottom: 0;">
-    <span style="color: #404040">{{ config('app.name') }} | </span>{{ config('mail.from.username') }}
+   {{ config('app.name') }}
 </p>
 <p style="color:#404040;max-width: 400px;text-wrap: break-word; padding-bottom: 0;margin-bottom: 0;">{{ \App\Models\SystemDetail::find(1)?->address }}</p>
 <p style="color:#404040;max-width: 300px;text-wrap: break-word; padding-bottom: 0;margin-bottom: 0;"><a href="{{ \App\Models\SystemDetail::find(1)?->website }}" style="color:#404040;text-decoration:underline" target="_blank">
